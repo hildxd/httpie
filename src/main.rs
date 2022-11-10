028830e460e2ad2a14d818a991aaef7fa8109bdf
hildxd <hildxd@outlook.com>
2022-11-10 17:15:06 +0800

    feat: define command struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+/// a naive implementation of the `httpie` command
+#[derive(Parser, Debug)]
+#[command(version = "1.0", author = "sunlee")]
+struct Opt {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser, Debug)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+/// feed get with an url
+#[derive(Parser, Debug)]
+struct Get {
+    url: String,
+}
+
+#[derive(Parser, Debug)]
+struct Post {
+    url: String,
+    body: Vec<String>,
+}
 fn main() {
-    println!("Hello, world!");
+    let opts = Opt::parse();
+    println!("{:?}", opts);
 }
