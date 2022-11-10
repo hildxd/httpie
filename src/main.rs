1948c17451d2731d642b2a37278cf818675b24ef
SunLee <hildxd@outlook.com>
2022-11-10 23:59:33 +0800

    feat: log response info

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use anyhow::{anyhow, Ok, Result};
 use clap::Parser;
 use mime::Mime;
 use reqwest::{header, Client, Url, Response};
+use syntect::{
+    easy::HighlightLines,
+    highlighting::{Style, ThemeSet},
+    parsing::SyntaxSet,
+    util::{as_24_bit_terminal_escaped, LinesWithEndings},
+};
+use console::style;
 
 /// a naive implementation of the `httpie` command
 #[derive(Parser, Debug)]
@@ -55,8 +62,7 @@ fn parse_url(s: &str) -> Result<String> {
 
 async fn get(client: Client, args: &Get) -> Result<()> {
     let resp = client.get(&args.url).send().await?;
-    println!("{:?}", resp);
-    Ok(())
+    Ok(print_resp(resp).await?)
 }
 
 async fn post(client: Client, args: &Post) -> Result<()> {
@@ -69,13 +75,30 @@ async fn post(client: Client, args: &Post) -> Result<()> {
         .json(&body)
         .send()
         .await?;
-    println!("{:?}", resp);
-    Ok(())
+    Ok(print_resp(resp).await?)
+}
+
+// 打印服务器版本号 + 状态码
+fn print_status(resp: &Response) {
+    let status = style(format!("{:?} {}", resp.version(), resp.status())).blue();
+    println!("{}\n", status);
+}
+
+// 打印服务器返回的 HTTP header
+fn print_headers(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", style(name.to_string()).green(), value);
+    }
+
+    println!();
 }
 
 async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_headers(&resp);
     let mime = get_content_type(&resp);
     let body = resp.text().await?;
+    print_body(mime, &body);
     Ok(())
 }
 
@@ -84,6 +107,31 @@ fn get_content_type(resp: &Response) -> Option<Mime> {
     resp.headers()
         .get(header::CONTENT_TYPE)
         .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+/// 打印服务器返回的 HTTP body
+fn print_body(m: Option<Mime>, body: &str) {
+    match m {
+        // 对于 "application/json" 我们 pretty print
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
+        Some(v) if v == mime::TEXT_HTML => print_syntect(body, "html"),
+
+        // 其它 mime type，我们就直接输出
+        _ => println!("{}", body),
+    }
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(s) {
+        let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ps).unwrap();
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
 }
 
 #[tokio::main]
